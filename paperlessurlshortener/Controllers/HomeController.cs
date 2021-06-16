90975d779640fcef5498f55107af821948a6467b
Eric Patrick <epatrick@inspectiondepot.com>
2021-06-16 15:48:23 -0400

    Add project files.

---
+++ paperlessurlshortener/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using paperlessurlshortener.Models;
+
+namespace paperlessurlshortener.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class URLResponse
+        {
+            public string url { get; set; }
+            public string status { get; set; }
+            public string token { get; set; }
+        }
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
